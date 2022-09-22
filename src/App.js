f4bc1c7e2d70fa9fe12cfcb3c593683ae726a05f
Margherita33 <lucy89mar@gmail.com>
2022-09-22 22:42:25 +0200

    component counter using the state hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
-import Counter from "./components/Counter.js";
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter.js';
 
 const App = () => {
   return (
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,17 @@
-import { render } from "@testing-library/react";
 import React, { useState } from "react";
-
-import App from "./App";
 
 import "./Counter.css";
 
 function Counter() {
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setCount(count - 1)}>Click me</button>
     </div>
   );
 }
+
+export default Counter;
